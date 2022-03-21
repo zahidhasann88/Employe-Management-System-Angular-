8e16a66032d845fcfb16697b8d6ad10d076de6f1
Zahid Hasan <jahidhasann67@gmail.com>
2022-03-20 21:31:56 -0700

    first commit

---
+++ API/WebApi/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int EmployeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
